38d0847128fd540fec1e2bb1e61fd57607dad5d1
PhaleWhale <84545174+PhaleWhale@users.noreply.github.com>
2023-06-21 19:44:27 +1000

    cookoe

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -1,24 +1,23 @@
 function setMode(mode) {
-	const stylesheet = document.getElementById("cookieMode");
-	if (mode === 'dark') {
-		stylesheet.href = "./src/css/darkmode.css";
-	} 
-	else if (mode === 'light') {
-		stylesheet.href = "./src/css/lightmode.css";
-	}
-	// Save the mode value in a cookie
-	document.cookie = `mode=${mode}; path=/`;
+  const body = document.querySelector('body');
+  if (mode === 'dark') {
+    body.classList.add('dark-mode');
+    body.classList.remove('light-mode');
+  } else if (mode === 'light') {
+    body.classList.add('light-mode');
+    body.classList.remove('dark-mode');
+  }
+  // Save the mode value in a cookie
+  document.cookie = `mode=${mode}; path=/`;
 }
 
 function checkModeCookie() {
-	const cookieValue = document.cookie
-	.split('; ')
+  const cookieValue = document.cookie
+    .split('; ')
     .find(row => row.startsWith('mode='))
     ?.split('=')[1];
 
-	if (cookieValue) {
-		setMode(cookieValue);
-	}
+  if (cookieValue) {
+    setMode(cookieValue);
+  }
 }
-
-checkModeCookie();
