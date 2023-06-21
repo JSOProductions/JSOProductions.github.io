4e3b0fb9997459bfbccede6259a3a6e3c95eaf09
JSOProductions <71542525+JSOProductions@users.noreply.github.com>
2023-06-21 19:06:37 +1000

    Cookie Work

---
+++ src/js/cookie.js
@@ -0,0 +1,25 @@
+function setMode(mode) {
+  const body = document.querySelector('body');
+  if (mode === 'dark') {
+    body.classList.add('dark-mode');
+    body.classList.remove('light-mode');
+  } else if (mode === 'light') {
+    body.classList.add('light-mode');
+    body.classList.remove('dark-mode');
+  }
+  // Save the mode value in a cookie
+  document.cookie = `mode=${mode}; path=/`;
+}
+
+function checkModeCookie() {
+  const cookieValue = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('mode='))
+    ?.split('=')[1];
+
+  if (cookieValue) {
+    setMode(cookieValue);
+  }
+}
+
+checkModeCookie();
