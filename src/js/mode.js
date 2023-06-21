277e4e2615acd9174ca3a5d6853246697615087b
JSOProductions <71542525+JSOProductions@users.noreply.github.com>
2023-06-21 19:30:23 +1000

    push time

--- src/js/mode.js
+++ src/js/mode.js
@@ -1,13 +1,13 @@
 function mode() {
  
 	/* Variables */
-	const stylesheet = document.getElementById("darkMode");
+	const stylesheet = document.getElementById("cookieMode");
 	var changeVar = document.getElementById("change");
 	
 	/* Action */
 	if (changeVar.checked == true) {
-		stylesheet.href = "./src/css/darkmode.css";
+		
 	} else {
-		stylesheet.href = "./src/css/lightmode.css";
+		
 	}
 }
