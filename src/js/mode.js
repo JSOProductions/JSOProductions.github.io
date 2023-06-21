5626a8273ab127236c222e846f5984df33312526
PhaleWhale <84545174+PhaleWhale@users.noreply.github.com>
2023-06-21 21:53:52 +1000

    got dark mode working

--- src/js/mode.js
+++ src/js/mode.js
@@ -1,32 +1,41 @@
-function mode() {
-  const stylesheet = document.getElementById("cookieMode");
-  const mode = document.getElementById("change");
-  const body = document.querySelector('body');
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode'); 
 
-    if (mode === 'dark') {
-    body.classList.add('dark-mode');
-    body.classList.remove('light-mode');
-  } else if (mode === 'light') {
-    body.classList.add('light-mode');
-    body.classList.remove('dark-mode');
-  }
-  // Save the mode value in a cookie
-  document.cookie = `mode=${mode}; path=/`;
-	if (body === 'dark-mode') {
-    stylesheet.href = "./src/css/darkmode.css";
-  } else {
-    stylesheet.href = "./src/css/lightmode.css";
-  }
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const stylesheet = document.getElementById("cookieMode");
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('darkmode');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('darkMode', 'enabled');
+  stylesheet.href = "./src/css/darkmode.css";
 }
 
-
-function checkModeCookie() {
-  const cookieValue = document.cookie
-    .split('; ')
-    .find(row => row.startsWith('mode='))
-    ?.split('=')[1];
-
-  if (cookieValue) {
-    setMode(cookieValue);
-  }
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('darkmode');
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('darkMode', null);
+  stylesheet.href = "./src/css/lightmode.css";
 }
+ 
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('darkMode'); 
+  
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  // if it has been enabled, turn it off  
+  } else {  
+    disableDarkMode(); 
+  }
+});
