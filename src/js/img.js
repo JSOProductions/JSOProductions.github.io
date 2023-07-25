94aec79a48453bea164045a430a85bfb02859834
PhaleWhale <84545174+PhaleWhale@users.noreply.github.com>
2023-07-25 12:12:21 +1000

    made images good

--- src/js/img.js
+++ src/js/img.js
@@ -40,7 +40,48 @@ function showSlides(n) {
 }
 
 window.onkeydown = function (e) {
-	if (e.key == 'Escape'){closeModal()}
-	if (e.keyCode == '37') {plusSlides(-1)}
-	if (e.keyCode == '39') {plusSlides(1)}
+	if (e.key == 'Escape'){closeModal(); closeModal2()}
+	if (e.keyCode == '37') {plusSlides(-1); plusSlides2(-1)}
+	if (e.keyCode == '39') {plusSlides(1); plusSlides2(1)}
+}
+
+// Open the Modal
+function openModal2() {
+  document.getElementById("myModal2").style.display = "block";
+}
+
+// Close the Modal
+function closeModal2() {
+  document.getElementById("myModal2").style.display = "none";
+}
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  var i;
+  var slides2 = document.getElementsByClassName("mySlides2");
+  var dots2 = document.getElementsByClassName("demo2");
+  var captionText2 = document.getElementById("caption2");
+  if (n > slides2.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides2.length}
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";
+  }
+  for (i = 0; i < dots2.length; i++) {
+    dots2[i].className = dots2[i].className.replace(" active2", "");
+  }
+  slides2[slideIndex2-1].style.display = "block";
+  dots2[slideIndex2-1].className += " active2";
+  captionText2.innerHTML = dots2[slideIndex2-1].alt;
 }
